af75f51f37bd3659221c7106a915665af594fb3c
shalini-singhal <122008550+shalini-singhal@users.noreply.github.com>
2025-10-30 11:09:16 +0530

    updated

--- main.py
+++ main.py
@@ -50,5 +50,19 @@ def takeCommand():
 
 
 if __name__ == "__main__":
-   takeCommand()
 
+    query = takeCommand().lower()
+
+if "wikipedia" in query:
+    speak("Searching Wikipedia...")
+    query = query.replace('wikipedia', "")
+    results = wikipedia.summary(query, sentences = 2)
+    speak("According to wikipedia")
+    print(results)
+    speak(results)
+
+
+elif "youtube" in query:
+    speak("Opening YouTube")
+    webbrowser.open("youtube.com")
+    
